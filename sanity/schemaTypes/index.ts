c2f06f5d2c2f764bb63e37033d7494c6a20f73e8
makenasandra <makenawachira@gmail.com>
2025-01-08 23:16:02 +0300

    added sanity startups and author schemas

---
+++ sanity/schemaTypes/index.ts
@@ -0,0 +1,7 @@
+import { type SchemaTypeDefinition } from 'sanity'
+import {startup} from "@/sanity/schemaTypes/startup";
+import {author} from "@/sanity/schemaTypes/author";
+
+export const schema: { types: SchemaTypeDefinition[] } = {
+  types: [author, startup],
+}
