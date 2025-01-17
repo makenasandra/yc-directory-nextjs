033be1fbf2d5b3ed4108283dff5235dd9381e749
makenasandra <makenawachira@gmail.com>
2025-01-17 23:26:36 +0300

    added startup details form

--- auth.ts
+++ auth.ts
@@ -9,7 +9,7 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
   providers: [GitHub],
   callbacks: {
     async signIn({ 
-        user: {name, email, image}, 
+        user: {name , email, image}, 
         profile: {id, login, bio} }) {
       const existingUser = await client
       .withConfig({useCdn: false})
