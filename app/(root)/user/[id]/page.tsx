bcdeb91b47962a03c01c251e341887abfa4da7ff
makenasandra <makenawachira@gmail.com>
2025-01-19 19:43:17 +0300

    updated next-config

--- app/(root)/user/[id]/page.tsx
+++ app/(root)/user/[id]/page.tsx
@@ -1,6 +1,6 @@
 import { auth } from '@/auth';
 import { StartupSkeleton } from '@/components/StartupCard';
-import UserStartups from '@/components/userStartups';
+import UserStartups from '@/components/UserStartups';
 import { client } from '@/sanity/lib/client';
 import { AUTHOR_BY_ID_QUERY } from '@/sanity/lib/queries';
 import Image from 'next/image';
